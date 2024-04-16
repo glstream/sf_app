41fa94e1f28378a8954f3a7d47bef3b278655c8e
glstream <grayson.stream@gmail.com>
2024-04-16 10:28:07 -0700

    header toast

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,25 +5,26 @@
     <a-menu theme="dark" mode="horizontal" :default-selected-keys="['1']">
       <a-menu-item key="1"><router-link to="/username">Home</router-link></a-menu-item>
       <a-menu-item key="3"
-        ><router-link to="/tradecalculator"
+        ><router-link to="/tradecalculator" class="menu-item-link"
           ><span class="pi pi-calculator"></span> Trade Calculator</router-link
         ></a-menu-item
       >
-      <a-menu-item key="2"
-        ><router-link :to="leaguesUrl"
+      <a-menu-item key="2" @click="handleHeaderClick"
+        ><router-link :to="leaguesUrl" class="menu-item-link"
           ><span class="pi pi-th-large"></span> Leagues</router-link
         ></a-menu-item
       >
 
-      <a-menu-item key="4"
-        ><router-link to="/ranks"
-          ><span class="pi pi-sort-amount-down"></span> Ranks</router-link
-        ></a-menu-item
-      >
+      <a-menu-item key="4">
+        <router-link to="/ranks" class="menu-item-link">
+          <span class="pi pi-sort-amount-down"></span>
+          <span>Ranks</span>
+        </router-link>
+      </a-menu-item>
 
       <a-menu-item key="5"
-        ><router-link to="/about"
-          ><span class="pi pi-question-circle'"></span> About</router-link
+        ><router-link to="/about" class="menu-item-link"
+          ><span class="pi pi-question-circle"></span> About</router-link
         ></a-menu-item
       >
     </a-menu>
@@ -31,15 +32,23 @@
 </template>
 
 <script setup>
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import logoImg from '@/assets/logo5.png'
 
 import { useUserStore } from '@/stores/userStore'
 import { CalculatorOutlined } from '@ant-design/icons-vue'
+import { message } from 'ant-design-vue'
 import 'primeicons/primeicons.css'
 
 const store = useUserStore()
 const leaguesUrl = computed(() => `/leagues/${store.leagueYear}/${store.userName}/${store.guid}`)
+const showPrompt = ref(false)
+
+const handleHeaderClick = () => {
+  if (!store.userName) {
+    message.warning('Please fill out your username.') // Show a warning message if username is not set
+  }
+}
 </script>
 
 <style scoped>
@@ -55,5 +64,14 @@ const leaguesUrl = computed(() => `/leagues/${store.leagueYear}/${store.userName
 
 [data-theme='dark'] .site-layout .site-layout-background {
   background: #141414;
+}
+.menu-item-link {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.pi {
+  margin-right: 5px;
 }
 </style>
