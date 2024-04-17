c85e296b86ce01cd47ac6eee3b0fd3434c6e36ed
glstream <grayson.stream@gmail.com>
2024-04-16 21:54:56 -0700

    performance4

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -2,10 +2,8 @@
   <a-layout-footer style="text-align: center; padding-top: 10px">
     Dynasty SuperFlex ©2024
     <br />
-    <a href="https://twitter.com/fantasynav1" target="_blank" rel="noopener noreferrer"> X </a>
+    <a href="https://twitter.com/fantasynav1" target="_blank"> X </a>
     |
-    <a href="https://www.buymeacoffee.com/superflex" target="_blank" rel="noopener noreferrer">
-      Donate
-    </a>
+    <a href="https://www.buymeacoffee.com/superflex" target="_blank"> Donate </a>
   </a-layout-footer>
 </template>
