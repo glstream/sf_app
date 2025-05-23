3ad20dc464bc6219326030f3bc2e2f7df4fb2370
glstream <grayson.stream@gmail.com>
2025-05-22 18:46:50 -0700

    new vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // Explicitly set the base path
   plugins: [vue()],
   resolve: {
     alias: {
