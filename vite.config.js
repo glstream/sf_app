1e82fa455bfb83d0141df40f6b3b1ad05dbc14e9
glstream <grayson.stream@gmail.com>
2025-05-22 20:55:34 -0700

    remove bundle

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: './',
+  base: '/',
   build: {
     assetsDir: 'assets',
     sourcemap: true,
